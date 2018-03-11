59fcc5c36cc6406fe90a2edd69caabf595ffdaee
cjh <only_freedom@163.com>
2018-03-11 17:17:03 +0800

    add a line for test git

--- src/org/nt/think1/Demo2.java
+++ src/org/nt/think1/Demo2.java
@@ -1,10 +1,9 @@
 package org.nt.think1;
 
-import java.util.Formatter;
-
 public class Demo2 {
     public static void main(String[] args) {
         String s = String.format("ok, fine");
         System.out.println(s);
+        System.out.println(String.format("我的幸运数%d", 2));
     }
 }
